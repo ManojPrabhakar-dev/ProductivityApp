de5df48389ad52d3ced29a041b3abac765310065
Mprabhak <Manoj.prabhakars@analog.com>
2021-11-17 12:49:49 +0530

    Added line-through for checked changed

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TaskItem = (props) => {
+  const [isDone, setDone] = useState(false);
+
+  function handleCheckedChanged(event) {
+    setDone(event.target.checked);
+  }
+
   function handleDelete(event) {
     props.onDelete(props.taskName);
   }
   return (
     <div className="taskItem">
-      <p className="taskName"> {props.taskName} </p>
+      <input type="checkbox" onChange={handleCheckedChanged} />
+      <p className={`taskName ${isDone && "done"}`}> {props.taskName} </p>
       <button className="deleteTask" onClick={handleDelete}>
         Delete
       </button>
